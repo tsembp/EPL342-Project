e5c6acf6e6f79a96438e566ea5b1c29243530eed
Panagiotis Tsembekis <panagiotistsembekis45@gmail.com>
2025-11-19 22:40:08 +0200

    1st round DB programming for signup

---
+++ db_programming/sproc/signup/driver_comprep_documents.sql
@@ -0,0 +1,110 @@
+CREATE OR ALTER PROCEDURE dbo.usp_AddPersonDocument
+(
+    @UserId     UNIQUEIDENTIFIER,
+    @DocType    NVARCHAR(100),
+    @DocNumber  NVARCHAR(100),
+    @IssueDate  DATETIME2,
+    @ExpiryDate DATETIME2 = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Validate user type
+    IF NOT EXISTS (
+        SELECT 1
+        FROM [dbo].[User] U
+        WHERE U.UserId = @UserId
+          AND U.Role IN ('D','C')
+    )
+    BEGIN
+        RAISERROR('User does not exist or is not a Driver/Company Representative.', 16, 1);
+        RETURN;
+    END;
+
+    -- Validate DocType
+    IF @DocType NOT IN (
+        'ID_OR_PASSPORT',
+        'RESIDENCE_PERMIT',
+        'DRIVING_LICENSE',
+        'VEHICLE_REG',
+        'MOT_CERT',
+        'CRIMINAL_RECORD',
+        'MEDICAL_CERT',
+        'PSYCHOLOGICAL_CERT'
+    )
+    BEGIN
+        RAISERROR('Invalid DocType.', 16, 1);
+        RETURN;
+    END;
+
+    -- Validate dates
+    IF @IssueDate > GETUTCDATE()
+    BEGIN
+        RAISERROR('IssueDate cannot be in the future.', 16, 1);
+        RETURN;
+    END;
+
+    IF @ExpiryDate IS NOT NULL AND @ExpiryDate <= @IssueDate
+    BEGIN
+        RAISERROR('ExpiryDate must be NULL or later than IssueDate.', 16, 1);
+        RETURN;
+    END;
+
+    -- Validate uniqueness
+    IF EXISTS (
+        SELECT 1
+        FROM [dbo].[PersonDocument] PD
+        WHERE PD.UserId = @UserId
+          AND PD.DocType = @DocType
+          AND PD.DocNumber = @DocNumber
+    )
+    BEGIN
+        RAISERROR('Document with the same type and number already exists for this user.', 16, 1);
+        RETURN;
+    END;
+
+    IF EXISTS (
+        SELECT 1
+        FROM [dbo].[PersonDocument] PD
+        WHERE PD.DocNumber = @DocNumber
+    )
+    BEGIN
+        RAISERROR('Document with the same number already exists.', 16, 1);
+        RETURN;
+    END;
+
+    BEGIN TRY
+        INSERT INTO [dbo].[PersonDocument]
+        (
+            UserId,
+            DocType,
+            DocNo,
+            IssueDate,
+            ExpiryDate,
+            UploadedAt,
+            FileUrl
+        )
+        VALUES
+        (
+            @UserId,
+            @DocType,
+            @DocNumber,
+            @IssueDate,
+            @ExpiryDate,
+            GETUTCDATE(),
+            'https://storage-bucket.com/documents/' + CAST(@UserId AS NVARCHAR(36)) + '/' + @DocType + '_' + @DocNumber + '.pdf' -- dummy URL
+        );
+
+        -- Return the created doc
+        SELECT SCOPE_IDENTITY() AS DocId;
+    END TRY
+    BEGIN CATCH
+        DECLARE @ErrMsg NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrSev INT = ERROR_SEVERITY();
+        DECLARE @ErrState INT = ERROR_STATE();
+
+        RAISERROR(@ErrMsg, @ErrSev, @ErrState);
+    END CATCH;
+END;
+GO
---
+++ db_programming/sproc/signup/staff_signup.sql
@@ -0,0 +1,101 @@
+CREATE OR ALTER PROCEDURE dbo.usp_SignUpStaff
+(
+    @Role           CHAR(1),        -- 'O' : Operator, 'I': Inspector
+    @Email          NVARCHAR(MAX),
+    @Username       NVARCHAR(30),
+    @PasswordPlain  NVARCHAR(4000)
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Input validation
+    IF @Role NOT IN ('O','I')
+    BEGIN
+        RAISERROR('Invalid Role. Must be O or I.', 16, 1);
+        RETURN;
+    END;
+
+    -- Uniqueness check
+    IF EXISTS (SELECT 1 FROM [dbo].[Operator] WHERE Email = @Email)
+    OR EXISTS (SELECT 1 FROM [dbo].[Inspector] WHERE Email = @Email)
+    BEGIN
+        RAISERROR('Email already in use.', 16, 1);
+        RETURN;
+    END;
+
+    IF EXISTS (SELECT 1 FROM [dbo].[Operator] WHERE Username = @Username)
+    OR EXISTS (SELECT 1 FROM [dbo].[Inspector] WHERE Username = @Username)
+    BEGIN
+        RAISERROR('Username already in use.', 16, 1);
+        RETURN;
+    END;
+
+    -- Password hash
+    DECLARE @PasswordHash NVARCHAR(MAX) =
+        dbo.fn_HashPassword(@PasswordPlain);
+
+    DECLARE @UserId UNIQUEIDENTIFIER = NEWID();
+
+    BEGIN TRY
+        BEGIN TRAN;
+
+            IF @Role = 'O'
+            BEGIN
+                INSERT INTO [dbo].[Operator]
+                (
+                    OperatorId,
+                    Email,
+                    Username,
+                    PasswordHash,
+                    ApprovedByAdmin,
+                    ApprovedAt
+                )
+                VALUES
+                (
+                    @UserId,
+                    @Email,
+                    @Username,
+                    @PasswordHash,
+                    NULL, -- to be set when approved by an admin
+                    NULL -- to be set when approved by an admin
+                );
+            END
+            ELSE IF @Role = 'I'
+            BEGIN
+                INSERT INTO [dbo].[Inspector]
+                (
+                    InspectorId,
+                    Email,
+                    Username,
+                    PasswordHash
+                )
+                VALUES
+                (
+                    @UserId,
+                    @Email,
+                    @Username,
+                    @PasswordHash
+                );
+            END;
+
+        COMMIT TRAN;
+
+        -- Return created user
+        SELECT 
+            @UserId  AS UserId,
+            @Role    AS Role,
+            @Email   AS Email,
+            @Username AS Username;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK TRAN;
+
+        DECLARE @ErrMsg NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrSev INT = ERROR_SEVERITY();
+        DECLARE @ErrState INT = ERROR_STATE();
+
+        RAISERROR(@ErrMsg, @ErrSev, @ErrState);
+    END CATCH
+END;
+GO
